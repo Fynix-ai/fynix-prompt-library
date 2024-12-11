09aa1dff33870e9188527068a35332ffa1041075
ankur-helak <ankurhelak@gmail.com>
2024-12-11 14:50:16 +0530

    added icon, changed higlight color

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,17 +76,17 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Home',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Home Logo',
+          src: 'img/fynix-logo.png',
         },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Fynix Prompt Library',
           },
           
           {
